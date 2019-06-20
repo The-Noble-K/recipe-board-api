be942f708984e7781df7e29d769afea1ff56e42d
Jarod Sneed <jarodsneed@Jarods-MacBook.local>
2019-06-20 13:02:24 -0400

    add new recipe function

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,8 +1,18 @@
 module Api::V1
     class RecipesController < ApplicationController
         def index
-            @recipes = Recipe.all
+            @recipes = Recipe.order('created_at DESC')
             render json: @recipes
         end
+
+        def create
+            @recipe = Recipe.create(recipe_params)
+            render json: @recipe
+        end
+
+        private
+            def recipe_params
+                params.require(:recipe).permit(:title, :instructions, :ingredients)
+            end
     end
 end
